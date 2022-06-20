5fbc7bb1bedb413a12ef44d37e07903cbe7028fe
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2022-06-20 15:17:43 +0300

    Add design result prediction and recommendations

--- WebApplication/Models/PredictModel.cs
+++ WebApplication/Models/PredictModel.cs
@@ -5,13 +5,44 @@ using System.Threading.Tasks;
 
 namespace WebApplication.Models
 {
+    public enum DiabetesRisk 
+    {
+        Low,
+        Middle,
+        Hight
+    }
+
     public class PredictModel
     {
         public PredictModel(double probabilityDiabetes) 
         {
+            if (probabilityDiabetes > 1 || probabilityDiabetes < 0)
+                throw new ArgumentException("Значение вероятности должно быть в пределах от 0 до 1");
+
             ProbabilityDiabetes = probabilityDiabetes;
+            DiabetesRisk = AnalyzeProbability(probabilityDiabetes);
+            
         }
 
         public double ProbabilityDiabetes { get; }
+        public DiabetesRisk DiabetesRisk { get; }
+
+        private DiabetesRisk AnalyzeProbability(double probabilityDiabetes) 
+        {
+            DiabetesRisk diabetesRisk;
+            switch (probabilityDiabetes) 
+            {
+                case double value when (value <= 0.3):
+                    diabetesRisk = DiabetesRisk.Low;
+                    break;
+                case double value when (value <= 0.6):
+                    diabetesRisk = DiabetesRisk.Middle;
+                    break;
+                default:
+                    diabetesRisk = DiabetesRisk.Hight;
+                    break;
+            }
+            return diabetesRisk;
+        }
     }
 }
