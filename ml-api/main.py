b775e5b530e1e98e5fcc2c568ecc114c09c6a04e
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2022-06-20 12:06:26 +0300

    Add processing exception in api

--- ml-api/main.py
+++ ml-api/main.py
@@ -2,9 +2,6 @@ import numpy as np
 from flask import Flask, jsonify, request, abort
 from keras.models import load_model
 
-# TODO:
-# Добавить возможность обработки массива json-объектов ответов на вопросы для получения прогноза
-
 app = Flask(__name__)
 model = load_model('neural_model_pred_diabet.h5')
 name_attributes = ["HighBP", "HighChol", "CholCheck", "BMI", "Smoker", "Stroke", "HeartDiseaseorAttack", "PhysActivity",
@@ -15,7 +12,7 @@ name_attributes = ["HighBP", "HighChol", "CholCheck", "BMI", "Smoker", "Stroke",
 @app.route('/')
 def index():
     return '<h1>Welcome to API for predict diabetes</h1>' \
-           '<br><a href="https://github.com/AlexeyArtem/diabetes-predictor" target="_blank">Github link</a>'
+           '<br><a href="https://github.com/AlexeyArtem/diabetes-predictor" target="_blank">GitHub link</a>'
 
 
 @app.route('/predict', methods=['POST'])
@@ -23,21 +20,24 @@ def get_predict():
     if not request.json or not request.is_json:
         abort(400, 'The request content is not a json format')
 
-    # try:
-    data_json = request.get_json()
-    input_data = []
-    for attribute in name_attributes:
+    try:
+        data_json = request.get_json()
+        input_data = []
+        for attribute in name_attributes:
 
-        if attribute not in data_json:
-            abort(400, 'Key ' + attribute + ' not found in json request content')
+            if attribute not in data_json:
+                abort(400, 'Key ' + attribute + ' not found in json request content')
 
-        input_data.append(data_json[attribute])
+            input_data.append(data_json[attribute])
 
-    probabilities = model.predict(np.array([input_data]))
-    p = float(probabilities[0][1])
+        probabilities = model.predict(np.array([input_data]))
+        p = float(probabilities[0][1])
 
-    result = {'Probability_diabetes': p}
-    return jsonify(result)
+        result = {'Probability_diabetes': p}
+        return jsonify(result)
+
+    except:
+        abort(400)
 
 
 if __name__ == '__main__':
